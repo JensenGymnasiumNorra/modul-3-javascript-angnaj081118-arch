e4bb7c06f626445c47eba5561d7ece6aeaa86a9d
angnaj081118-arch <angnaj081118@student.jenseneducation.se>
2025-11-14 14:52:14 +0100

    hh

--- simple_calculator.js
+++ simple_calculator.js
@@ -1,6 +1,30 @@
-function calculateSum() {
-    let num1 = parseFloat(document.getElementById('num1').value);
-    let num2 = parseFloat(document.getElementById('num2').value);
-    let sum = num1 + num2;
-    document.getElementById('result').innerHTML = 'Result: ' + sum;
-}
+const num1 = document.getElementById('num1');
+const num1value = document.getElementById('num1value');
+
+num1.addEventListener('input', function() {
+num1value.textContent = num1.value;
+});
+
+const num2 = document.getElementById('num2');
+const num2value = document.getElementById('num2value');
+
+num2.addEventListener('input', function() {  
+num2value.textContent = num2.value;
+}); 
+
+const sell = document.getElementById('sell');
+const butt = document.getElementById('butt');
+
+butt.addEventListener('click', function() {
+     const num1 = parseFloat (document.getElementById('num1').value);
+     const num2 = parseFloat (document.getElementById('num2').value);
+   const AA = sell.value;
+   let result; 
+ 
+   if (AA === '+') result = num1 + num2; 
+   else if (AA === '-') result = num1 - num2; 
+   else if(AA === '/') result = num1 / num2;
+   else if(AA === '*') result = num1 * num2;
+
+   document.getElementById('result').textContent = 'result: ' + result;
+ }); 
--- uppg7.js
+++ uppg7.js
@@ -1,37 +1,118 @@
-// Här har du fått all js kod från genomgången
-// Den har självklart andra variabelnamn och annan funktionalitet än vad du behöver
-// Använ ddenna fil som utgångspunkt för att lösa uppgiften eller skriv din egen kod
+let Slider = document.getElementById('Slider');
+let Numba = document.getElementById('Numba');
+let Hint = document.getElementById('Hint');
+let p = document.getElementById('pp');
 
-let temperatureSlider = document.getElementById("temperatureSlider");
-let selectedTemperatureElement = document.getElementById("selectedTemperature");
-let weatherMessageElement = document.getElementById("weatherMessage");
+let answer1 = false;
+let answer2 = false;
+let answer3 = false; 
 
-// Initial display of selected temperature
-selectedTemperatureElement.innerText = temperatureSlider.value + "°C";
 
-// Initial update of weather message based on the default temperature
-updateWeatherMessage(temperatureSlider.value);
 
-// Event listener for slider change
-temperatureSlider.addEventListener("input", function() {
-    // Update the displayed temperature
-    selectedTemperatureElement.innerText = temperatureSlider.value + "°C";
+let wc = 0; 
+p.textContent = (`Win count: 0`);
 
-    // Update the weather message based on the selected temperature
-    updateWeatherMessage(temperatureSlider.value);
-});
+Numba.textContent = Slider.value; 
 
-// Function to update the weather message
-function updateWeatherMessage(temperature) {
-    if (temperature > 30) {
-        weatherMessageElement.innerText = "It's a hot day!";
-        weatherMessageElement.style.color = "red";
-    } else if (temperature <= 30 && temperature >= 20) {
-        weatherMessageElement.innerText = "The weather is pleasant.";
-        weatherMessageElement.style.color = "green";
-    } else {
-        weatherMessageElement.innerText = "It's a bit chilly.";
-        weatherMessageElement.style.color = "blue";
-    }
+let Ransom = Math.floor(Math.random() * 100) + 1;
+console.log(Ransom);
+
+Slider.addEventListener('input', () => {
+Numba.textContent = Slider.value; 
+
+let Wow = Number(Slider.value); 
+
+
+if (Wow === Ransom) {
+answer1 = true; 
+Hint.textContent = "Slider 1 is Correct!";
+} else if(Wow < Ransom){
+answer1 = true; 
+Hint.textContent = "Too low!"; 
+} else if (Wow > Ransom) { 
+    answer1 = true; 
+    Hint.textContent = "Too high!"; } }); 
+
+
+
+let Slider2 = document.getElementById('Slider2');
+let Numba2 = document.getElementById('Numba2');
+
+let Ransom2 = Math.floor(Math.random() * 100) + 1;
+console.log(Ransom2);
+
+
+
+Numba2.textContent =Slider2.value; 
+
+Slider2.addEventListener('input', () => {
+Numba2.textContent = Slider2.value; 
+
+let Wow2 = Number(Slider2.value); 
+
+if (Wow2 === Ransom2) {
+answer2 = true; 
+Hint.textContent = "Slider 2 is Correct!";
+} else if(Wow2 < Ransom2){
+answer2 = false; 
+Hint.textContent = "Too low!"; 
+} else if (Wow2 > Ransom2) {
+    answer2 = false; 
+    Hint.textContent = "Too high!"; } }); 
+
+
+
+
+let Slider3 = document.getElementById('Slider3');
+let Numba3 = document.getElementById('Numba3');
+
+let Ransom3 = Math.floor(Math.random() * 100) + 1;
+console.log(Ransom3);
+
+Numba3.textContent =Slider3.value; 
+
+Slider3.addEventListener('input', () => {
+Numba3.textContent = Slider3.value; 
+
+let Wow3 = Number(Slider3.value); 
+
+if (Wow3 === Ransom3) {
+    answer3 = true; 
+Hint.textContent = "Slider 3 is Correct!";
+} else if(Wow3 < Ransom3){
+answer3 = false; 
+Hint.textContent = "Too low!"; 
+} else if (Wow3 > Ransom3) { 
+    answer3 = false; 
+    Hint.textContent = "Too high!"; } });
+
+
+
+let NewNumba = document.getElementById('NewNumba');
+
+NewNumba.addEventListener('click', () => {
+if (answer1 && answer2 && answer3) {
+Hint.textContent = "Number has been changed!";
+wc++; 
+p.textContent = (`Win count: ${wc}`);
+Slider.max = Number(Slider.max) + 100;
+console.log(Slider.max);
+Ransom = Math.floor(Math.random() * Slider.max) + 1;
+Slider2.max = Number(Slider2.max) + 100;
+console.log(Slider2.max);
+Ransom2 = Math.floor(Math.random() * Slider2.max) + 1;
+Slider3.max = Number(Slider3.max) + 100;
+console.log(Slider3.max);
+Ransom3 = Math.floor(Math.random() * Slider3.max) + 1;
+} else {
+Hint.textContent = "You need to guess the right number on all sliders to change!"
 }
+}); 
+
+
+
+
+
+
+
 
